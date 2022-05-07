e7dc5dae7fef0997b5f9a67540d8b82e4574c92e
Mariya Ivonina <lobashova.mariya@mail.ru>
2022-05-08 00:17:19 +0300

    feat: added element on page and added basic css style

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,24 @@
 import keys from "./keys.js";
 
+const Keyboard = {
+  
+
+  download() {
+    const display = document.createElement('textarea');
+    display.classList.add('display');
+    document.querySelector('body').append(display);
+
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('keyboard__wrapper');
+    document.querySelector('body').append(wrapper);
+
+    const key = document.createElement('button');
+    key.classList.add('keyboard__key');
+    wrapper.append(key);
+  },
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  Keyboard.download();
+})
+
