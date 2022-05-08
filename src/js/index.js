fb4693dc688963476b559e54bf0d6b9d999f060a
Mariya Ivonina <lobashova.mariya@mail.ru>
2022-05-09 00:23:40 +0300

    feat: added code for btns keydown and keyup

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 import keybordKeys from "./keys.js";
 
 const Keyboard = {
-  
+  language: 'en',
+  isCapsLock: 'false',
+  btns: [],
 
   download() {
     const display = document.createElement('textarea');
@@ -11,17 +13,42 @@ const Keyboard = {
     const wrapper = document.createElement('div');
     wrapper.classList.add('keyboard__wrapper');
 
-    const keys = keybordKeys.map(el => {
-      console.log(el.contentLang.en);
+    this.btns = keybordKeys.map(el => {
       let key = document.createElement('button');
       key.classList.add('keyboard__key');
+      key.id = el.name;
       key.textContent = el.contentLang.en;
       wrapper.append(key);
+      this.showDisplay(key);
       return key;
     } )
     document.body.append(wrapper);
+    this.pressBtn(this.btns);
   },
-  
+
+  pressBtn(btns) {
+    document.addEventListener('keydown', function(event){
+      btns.forEach(el => {
+      if(event.code === el.id) {
+          el.classList.add('active');
+        }
+      })
+    });
+    document.addEventListener('keyup', function(event){
+      btns.forEach(el => {
+      if(event.code === el.id) {
+          el.classList.remove('active');
+        }
+      })
+    });
+  },
+
+  showDisplay(key) {
+    key.addEventListener('click', (event) => {
+
+    })
+  }
+
 }
 
 window.addEventListener('DOMContentLoaded', () => {
