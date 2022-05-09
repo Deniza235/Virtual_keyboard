41e5b08d573746e4b4a95674c532201976d066ec
Mariya Ivonina <lobashova.mariya@mail.ru>
2022-05-09 17:24:40 +0300

    feat: added function showDisplay

--- src/js/index.js
+++ src/js/index.js
@@ -3,15 +3,21 @@ import keybordKeys from "./keys.js";
 const Keyboard = {
   language: 'en',
   isCapsLock: 'false',
+  display: null,
   btns: [],
 
   download() {
-    const display = document.createElement('textarea');
-    display.classList.add('display');
-    document.body.append(display);
+    this.display = document.createElement('textarea');
+    this.display.classList.add('display');
+    this.display.setAttribute('autofocus', true);
+    document.body.append(this.display);
 
     const wrapper = document.createElement('div');
     wrapper.classList.add('keyboard__wrapper');
+
+    const descr = document.createElement('p');
+    descr.classList.add('descr');
+    descr.textContent = 'Клавиатура создана в операционной системе Windows';
 
     this.btns = keybordKeys.map(el => {
       let key = document.createElement('button');
@@ -21,9 +27,13 @@ const Keyboard = {
       wrapper.append(key);
       this.showDisplay(key);
       return key;
-    } )
+    })
     document.body.append(wrapper);
+    document.body.append(descr);
     this.pressBtn(this.btns);
+    this.display.addEventListener('blur', (e) => {
+      e.target.focus();
+  });
   },
 
   pressBtn(btns) {
@@ -45,13 +55,37 @@ const Keyboard = {
 
   showDisplay(key) {
     key.addEventListener('click', (event) => {
-
+      let word = '';
+      for(let i = 0; i < keybordKeys.length; i ++) {
+        if(keybordKeys[i].name === event.target.id) {
+          if(this.isCapsLock) {
+            word = keybordKeys[i].contentLang['en'];
+          } else {
+            word = keybordKeys[i].contentShift['en'];
+          }
+        }
+      }
+      this.display.setRangeText(word, this.display.selectionStart, this.display.selectionEnd, 'end');
     })
-  }
+    
+  },
+
+  /* setLocalStorage() {
+    localStorage.setItem(this.language);
+  },
+
+  getLocalStorage() {
+    if(localStorage.getItem(this.language)) {
+      this.language = localStorage.getItem(this.language);
+    }
+  }, */
 
 }
 
 window.addEventListener('DOMContentLoaded', () => {
   Keyboard.download();
 })
+
+/* window.addEventListener('beforeunload', Keyboard.setLocalStorage);
+window.addEventListener('load', Keyboard.getLocalStorage); */
 
